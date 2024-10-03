4de70b4f9029eae56cf82622a99794211a195a3f
fmings <mingsfelicia@gmail.com>
2024-10-03 16:57:28 -0500

    set up models

--- Models/Book.cs
+++ Models/Book.cs
@@ -2,5 +2,14 @@
 {
     public class Book
     {
+        public string Id { get; set; }
+        public string AuthorId { get; set; }
+        public string Title { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public bool Sale { get; set; }
+        public string UserId { get; set; }
+        public Author Author { get; set; }
     }
 }
