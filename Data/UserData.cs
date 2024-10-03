29dda8b2409adcde6fd06a74aad0223a09b27ae5
fmings <mingsfelicia@gmail.com>
2024-10-03 17:47:33 -0500

    set up mock data

--- Data/UserData.cs
+++ Data/UserData.cs
@@ -1,6 +1,18 @@
-﻿namespace SimplyBooksAPI.Data
+﻿using SimplyBooksAPI.Models;
+
+namespace SimplyBooksAPI.Data
 {
     public class UserData
     {
+        public static List<User> Users = new List<User>
+        {
+            new User
+            {
+                Id = "201",
+                FirstName = "Felicia",
+                LastName = "Mings",
+                Email = "mingsfelicia@gmail.com",
+            },
+        };
     }
 }
